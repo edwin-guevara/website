8d456901ea6ea63b62da111c9f4da0a9706bfe66
edwin-guevara <24eguevara@gmail.com>
2024-09-21 12:32:25 -0400

    Resume.js

--- Resume.js
+++ Resume.js
@@ -1,9 +1,21 @@
-import ResumeItem from "../components/ResumeItem";
+ import ResumeItem from "../components/ResumeItem";
 
-export default function Website() {
-  return (
-    <>
-    <h1>Vapor</h1>
-    </>
-    );
-}
+ export default function Website() {
+   return (
+     <>
+        <h1>Vapor</h1>
+        <ResumeItem
+          name = "athenahealth"
+          dept = "Healthcare IT"
+          position = "Member of Technical Staff"
+          startDate = {2017}
+          endDate = {2019}
+        >
+          <li>
+            Developed widgets using React and Less to display patient information.
+         </li>
+       </ResumeItem>
+       <ul></ul>
+     </>
+   );
+ }
