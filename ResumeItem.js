89e5697bb988e2fb2b1fa0ff175ea78db1a0053f
edwin-guevara <24eguevara@gmail.com>
2024-09-21 12:38:57 -0400

    ResumeItem.js

--- ResumeItem.js
+++ ResumeItem.js
@@ -9,13 +9,13 @@ export default function ResumeItem({
   return (
     // Paste the resume item <section> HTML here over lines 10 and 11
     <section>
-    <h3>
-    {name} | {dept}
-    </h3>
-  <h4>
-    {position} <time>{startDate}</time> - <time>{endDate}</time>
-    </h4>{" "}
-  <ul>{children}</ul>
+      <h3>
+        {name} | {dept}
+      </h3>
+      <h4>
+        {position} <time>{startDate}</time> - <time>{endDate}</time>
+      </h4>{" "}
+      <ul>{children}</ul>
     </section>
   );
 }
